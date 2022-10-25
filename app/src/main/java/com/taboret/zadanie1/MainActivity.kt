84973e82c4e7cee8c71ac97c0da4b815e8c7538f
student-14 <terleckawika123@gmail.com>
2022-10-25 10:23:55 +0200

    v3

--- app/src/main/java/com/taboret/zadanie1/MainActivity.kt
+++ app/src/main/java/com/taboret/zadanie1/MainActivity.kt
@@ -2,10 +2,32 @@ package com.taboret.zadanie1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var replaceButton = findViewById<Button>(R.id.button2);
+        var addButton = findViewById<Button>(R.id.button);
+        var nameLabel = findViewById<TextView>(R.id.textView);
+        var nameTextbox = findViewById<TextInputEditText>(R.id.pole);
+
+        addButton.setOnClickListener {
+            nameLabel.text = nameLabel.text.toString() + nameTextbox.text.toString();
+        };
+
+        replaceButton.setOnClickListener {
+            nameLabel.text = "Nicholas Fajer";
+        };
+     }
     }
+
+
+
+
 }
